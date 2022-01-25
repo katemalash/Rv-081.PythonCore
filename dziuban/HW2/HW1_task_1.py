d24c7dc85c9049f09776e9c5111e9b147bbb3a9a
Taras Dziuban <dziuban.taras1@gmail.com>
2022-01-25 21:34:54 +0200

    This is commit with home work 2

---
+++ dziuban/HW2/HW1_task_1.py
@@ -0,0 +1,8 @@
+#Task 1
+a = int(input("Enter number 'a' "))
+b = int(input("Enter number 'b' "))
+
+print(f"a + b = {a+b}")
+print(f"a - b = {a-b}")
+print(f"a * b = {a*b}")
+print(f"a / b = {a/b}")
---
+++ dziuban/HW2/HW1_task_2.py
@@ -0,0 +1,6 @@
+# Task_2
+name = input("What is your name? ")
+age = input("How old are you? ")
+address = input("Where do you live? ")
+
+print("Hello " + name + ", Your age is " + age + ", You live in " + address)
