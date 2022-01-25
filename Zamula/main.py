d07b8c989b156a2573bcc81c2566d67a3ff58612
NickZ99 <nick.zamula@gmail.com>
2022-01-25 13:36:31 +0200

    DZ-1

---
+++ Zamula/main.py
@@ -0,0 +1,26 @@
+#1
+
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+#2
+
+name = input("What is your name?")
+old = input("How old are you?")
+sity = input("Where do you live?")
+print("Hello, " + name + ",Your age is " + old + ",You live in " + sity)
+
+#3
+
+print("              ----------      ")
+print("            (            )    ")
+print("          (    ^      ^   )   ")
+print("         (        v        )  ")
+print("          (    \     /    )   ")
+print("           (    *****    )    ")
+print("             ------------     ")
