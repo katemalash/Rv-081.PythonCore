c291a7840ae91b3a1141b835e4e5e9854212d372
Arthur Limenberg <projektosoznanie@gmail.com>
2022-01-26 09:14:01 +0200

    Commit my homework

---
+++ Kozik/HW2/Task2.py
@@ -0,0 +1,10 @@
+name = input('What is your name?: ')
+age = input('How old are you?: ')
+city = input('Where do you live?: ')
+
+print(f'\nHallo, {name}\nYour age is {age}\nYou live in {city}')
+
+
+
+
+
