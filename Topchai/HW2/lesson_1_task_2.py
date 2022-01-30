5af0b38985882f4380592eb367e0ea396202ed53
Vladis138 <vladyslav.topchai@gmail.com>
2022-01-30 15:15:38 +0100

    Added my first homework

---
+++ Topchai/HW2/lesson_1_task_2.py
@@ -0,0 +1,11 @@
+name1 = input("What is your name?")
+age = input("How old are you?")
+city1 = input("Where do you leave?")
+name2 = name1 + ","
+city2 = city1 + "."
+
+for i in range(138):
+    print("Hello", name2, "your age is", age, "and you leave in", city2)
+    print("..................................................")
+
+# That's all)))
