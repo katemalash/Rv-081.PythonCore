e6a6dea78025d05b871719d2beae2dad9c40723b
Marianna Gripas <medvedulina@gmail.com>
2022-01-31 19:34:12 +0200

    домашнее задание с первого урока

---
+++ Gripas/HW2/task 1 Gripas M.py
@@ -0,0 +1,20 @@
+#task 1
+#slogenie
+a = float (input("введите первое число: "))
+b = float (input ("введите второе число: "))
+rezult1 = a + b
+print ("результат сложения " + str (rezult1))
+
+#vychitanie
+rezult2 = a - b
+print ("результат вычитания " + str (rezult2))
+
+#ymnogenie
+rezult3 = a * b
+print ("результат умножения " + str (rezult3))
+
+#delenie
+rezult4 = a / b
+print ("результат деления " + str (rezult4))
+
+
---
+++ Gripas/HW2/task 2 Gripas M.py
@@ -0,0 +1,10 @@
+#zadanie 2
+from types import CoroutineType
+
+
+name = input ("What is your name? ")
+age = input ("How old are you? ")
+city = input ("Where do you live? ")
+print ("Hello, " + name)
+print ("Your age is " + age)
+print ("You live in " + city)
