1e4ced5b9332d3487047c56ea35081fb1bbad08d
Taras Dziuban <dziuban.taras1@gmail.com>
2022-01-31 23:41:57 +0200

    This is HW 3

---
+++ dziuban/HW3/HW3_task4.py
@@ -0,0 +1,7 @@
+circle_radius = int(input("Enter the radius of the circle "))
+
+area = 3.14 *  (circle_radius ** 2)
+perimeter = 2 * (3.14 * circle_radius)
+
+print(f"Area = {area}")
+print(f"Perimeter = {perimeter}")
