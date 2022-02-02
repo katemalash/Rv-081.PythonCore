c5127337f71057049e6fbeefb03184d252598741
Sasha Moroz <morozop1986@gmail.com>
2022-02-02 16:41:19 +0200

    My HW3

---
+++ task1.py
@@ -0,0 +1,4 @@
+a = int(input('Please enter value a: '))
+b = int(input('Please enter value b: '))
+a,b = b,a
+print(f'Swap the values: a - {a}, b - {b}')	
---
+++ task2.py
@@ -0,0 +1,4 @@
+b = int(input('Please enter information in bytes: '))
+kb = b / 1024
+mb = kb / 1024
+print(f'Values in kilobytes: {kb} ,\nValues in megabytes: {mb}')
