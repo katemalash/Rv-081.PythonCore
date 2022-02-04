45cd09474d713b60ec059123d66d5ee93bc2082e
Taras Dziuban <dziuban.taras1@gmail.com>
2022-02-04 17:35:20 +0200

    t wit HW3 Commit to HW3 this is HW_3

--- dziuban/HW3/HW3_task2.py
+++ dziuban/HW3/HW3_task2.py
@@ -1,6 +1,6 @@
 in_bytes = int(input("Enter amount in bytes "))
 
 in_kilobytes = in_bytes/1024
-in_megabytes = in_bytes/1048576
+in_megabytes = (in_bytes/1024) / 1024
 
 print(f"It will be {in_kilobytes} kilobytes, or {in_megabytes} megabytes")
--- dziuban/HW3/HW3_task4.py
+++ dziuban/HW3/HW3_task4.py
@@ -1,7 +1,8 @@
 circle_radius = int(input("Enter the radius of the circle "))
+pi = 3.14
 
-area = 3.14 *  (circle_radius ** 2)
-perimeter = 2 * (3.14 * circle_radius)
+area = pi * (circle_radius**2)
+perimeter = 2 * (pi*circle_radius)
 
 print(f"Area = {area}")
 print(f"Perimeter = {perimeter}")
