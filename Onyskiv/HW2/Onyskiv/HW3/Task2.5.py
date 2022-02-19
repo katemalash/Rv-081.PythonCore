9b946322fc0aba6338e758a8ffc1381353595587
Andriy Onyskiv <onyskiv@gmail.com>
2022-02-19 19:40:56 +0200

    second homework

---
+++ Onyskiv/HW2/Onyskiv/HW3/Task2.5.py
@@ -0,0 +1,9 @@
+side1=int(input('Enter the length of the first side of the triangle:'))
+side2=int(input('Enter the length of the second side of the triangle:'))
+
+print('first side=',side1,' second side=',side2)
+
+hypotenuse=(side1**2+side2**2)**0.5
+
+print('hypotenuse=',hypotenuse)
+
