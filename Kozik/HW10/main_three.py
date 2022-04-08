3d0a309fd7157001005d3d720c3aaf13a51b7dba
Arthur Limenberg <projektosoznanie@gmail.com>
2022-04-08 16:27:51 +0300

    Homework 10

---
+++ Kozik/HW10/main_three.py
@@ -0,0 +1,6 @@
+from calculator import *
+
+print(summ(9, 5))
+print(product(7, 4))
+print(dif(0, 5))
+print(fraction(9, 3))
---
+++ Kozik/HW10/main_two.py
@@ -0,0 +1,4 @@
+from calculator import summ, dif
+
+print(summ(8, 9))
+print(dif(9, 6))
